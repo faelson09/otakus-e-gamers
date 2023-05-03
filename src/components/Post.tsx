5de2f180977f005d5aa6e83af08057368b496bda
faelson09 <nunesfaelson@gmail.com>
2023-05-03 05:55:37 +0100

    só por diversão

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -13,6 +13,7 @@ function Post({data}:props) {
   return (
     <div className='w-1/4 bg-white/0 px-3 mb-8 h-[350px] overflow-hidden '>
       <div className='h-full w-full rounded overflow-hidden relative'>
+        <div className={`absolute top-2 right-2 ${data.isbest == true? 'bg-green-600': ""}`}>pa</div>
         <img className='w-full h-full max-w-full hover:scale-150 hover:duration-300 duration-300 object-cover' src={data.url_da_image} alt='ola' width={400} height={600} />
         <p className='bg-black/30 text-lg text-white px-2 py-1 z-10 absolute bottom-0 w-full pointer-events-none capitalize'>{data.nome}</p>
       </div>
